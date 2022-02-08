af0f31235ecbf95cfdd271c1956578c1490b6f55
Nikhilsai10 <99253063+Nikhilsai10@users.noreply.github.com>
2022-02-08 19:24:48 +0530

    08-02-2022
    01. Python program on comparison opertaors
    01. Python program to convert Kilometers to centimeters

---
+++ comparison.py
@@ -0,0 +1,38 @@
+A = int(input())
+B = int(input())
+
+#checks whether the numbers are = or not
+if ( A == B ):
+   print ("A is equal to B")
+else:
+   print ("A is not equal to B")
+
+#checks whether the numbers are != or not
+if ( A != B ):
+   print ("A is not equal to B")
+else:
+   print ("A is equal to B")
+
+#checks whether the left hand's side value is greater than right hand's side 
+if ( A < B ):
+   print ("A is less than B") 
+else:
+   print ("A is not less than B")
+
+#checks whether the right hand's side value is greater than left hand's side 
+if ( A > B ):
+   print ("A is greater than B")
+else:
+   print ("A is not greater than B")
+
+#checks whether the left hand's side value is less than or =.
+if ( A <= B ):
+   print ("A is either less than or equal to  B")
+else:
+   print ("A is neither less than nor equal to  B")
+   
+#checks whether the left hand's side value is greater than or =.
+if ( B >= A ):
+   print ("A is either greater than  or equal to B")
+else:
+   print ("A is neither greater than  nor equal to B")
---
+++ kilometers.py
@@ -0,0 +1,4 @@
+#To convert Kilometers into Meters and Centimeters
+km = int (input("Enter the number of kilometers:"))
+cent = (km * 100000)
+print(km,"km is equals to ",cent,"centimeters")
